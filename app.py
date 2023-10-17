e27b3226230514acb3cc43287efb84334e2217d5
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-10-17 16:15:23 +0300

    index rote

--- app.py
+++ app.py
@@ -9,3 +9,10 @@ stripe_keys = {
     "pk_test_51O0EYTBSiu2J8WSY8BPlDOXRnwSB6X15HmG6znnnAL3wvT18QsaTt73NlvfTXUDhLpL632AKEWYHfMON54qLSWKE004AfEbyXM": os.environ.get("STRIPE_PUBLISHABLE_KEY")
 }
 
+stripe.api_key = stripe_keys["sk_test_51O0EYTBSiu2J8WSYEUrXqQgyznVyyJpcjjA8PqY2XaeH5ONeiiRUdt0q0VBCxSxQUYflBBil1YPh7RcaECqFy4Ln00k4RkJ4l1"]
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', key=stripe_keys['pk_test_51O0EYTBSiu2J8WSY8BPlDOXRnwSB6X15HmG6znnnAL3wvT18QsaTt73NlvfTXUDhLpL632AKEWYHfMON54qLSWKE004AfEbyXM'])
